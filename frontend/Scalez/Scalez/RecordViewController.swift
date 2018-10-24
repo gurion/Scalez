de2316ba71d8d68bd53f4716216f54f9ddeaebb4
Gurion Marks <gurion@jhu.edu>
2018-10-24 14:03:27 -0400

    updated frontend

---
+++ frontend/Scalez/Scalez/RecordViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RecordViewController.swift
+//  Scalez
+//
+//  Created by Gurion on 10/15/18.
+//  Copyright © 2018 OOSE. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RecordViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
