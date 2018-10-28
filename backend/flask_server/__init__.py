187fd96738e4fb536319becd9a6030a4d3c4d84d
unknown <arpadneale@gmail.com>
2018-10-27 23:30:35 -0400

    changed the init file to to something that resembles the flask mega tutorial

--- backend/flask_server/__init__.py
+++ backend/flask_server/__init__.py
@@ -1,39 +1,14 @@
 import os
 
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_server import user 
 
-def create_app(test_config=None):
-        #create and configure the app
-        app = Flask(__name__, instance_relative_config=True)
-        app.config.from_mapping(
-                SECRET_KEY = 'dev',
-                DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
-        )
-
-        if test_config is None:
-            # load the instance config, if it exists when not testing
-            app.config.from_pyfile('config.py', silent=True)
-        else:
-            # load the test config if passed in
-            app.config.from_mapping(test_config)
-
-        # ensure the instance of folder exists
-        try:
-            os.makedirs(app.instance_path)
-        except OSError:
-            pass
-
-        # a simple page that says hello
-        @app.route('/hello')
-        def hello():
-            return 'Hello, World!'
-
-        #added more code for the databases
-        from . import db
-        db.init_app(app)
-
-        from . import auth
-        app.register_blueprint(auth.bp)
-
-        return app
+app = FLask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+app.register_blueprint(user)
 
