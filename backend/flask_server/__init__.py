b0fd3e2d27f30b70eae20a7ba2a1afb10814e324
unknown <arpadneale@gmail.com>
2018-10-28 11:56:32 -0400

    Wrapped my head around blueprints and getting them to work in a modular fashion, check {host}/user/test and see a test message

--- backend/flask_server/__init__.py
+++ backend/flask_server/__init__.py
@@ -4,11 +4,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_server import user 
+from . import user 
 
-app = FLask(__name__)
+app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-app.register_blueprint(user)
+app.register_blueprint(user.bp)
+
+
 
