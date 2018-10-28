9a04cd398d2ca21d01a069efa3fdaebb2650d314
unknown <arpadneale@gmail.com>
2018-10-27 22:52:39 -0400

    created recording and scorer classes, started working setting up routes for a user

---
+++ backend/flask_server/user.py
@@ -0,0 +1,25 @@
+#this is a user blueprint to get started
+
+import functools
+
+#this is for setting up a blueprint which can be added to the app factory
+from flask import (
+        Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+#this is for authentication
+from wekzeug.security import check_password_hash, generate_password_hash
+
+bp = Blueprint('user', __name__, url_prefix='/auth')
+
+
+
+#the focus of this iteration is to send/score recordings
+#TODO:  add authentication with passwords
+#       check if users are actually in the database
+#       General Error Handeling
+
+@bp.route('/user', methods=('POST'))
+def new_user():
+#    if request.method == 'POST':
+
+
