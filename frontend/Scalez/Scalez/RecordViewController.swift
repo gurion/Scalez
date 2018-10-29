43dda16179a6f2860a7639981f4a880d9c6d0ea1
Eric Feldman <efeldm16@jhu.edu>
2018-10-28 23:13:08 -0400

    added an example post endpoint

--- frontend/Scalez/Scalez/RecordViewController.swift
+++ frontend/Scalez/Scalez/RecordViewController.swift
@@ -103,6 +103,38 @@ class RecordViewController: UIViewController, AVAudioRecorderDelegate {
             finishRecording(success: false)
         }
     }
+    
+    //posting something to a server
+    //this code came from https://github.com/Kilo-Loco
+    @IBAction func postOnTap(_ sender: Any) {
+        let parameters = ["username": "@kilo_loco", "tweet": "HelloWorld"]
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else { return }
+        request.httpBody = httpBody
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+            
+            }.resume()
+        
+    }
+    
 }
 
 
