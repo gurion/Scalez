2ce67c072cf991a7f2d2be747450fc1da75c890f
jacobsager <sagerjake@gmail.com>
2018-11-01 03:33:07 -0400

    Update add_user.py

--- backend/add_user.py
+++ backend/add_user.py
@@ -1,5 +1,5 @@
 from flask_server import db
 from flask_server.models import *
-u = User(username='Gurion', last='last', first='first', password_hash='password')
+u = User(username='Gurion', lastname='last', firstname='first', password_hash='password')
 db.session.add(u)
 db.session.commit()
