eb5d34767070f5ee4f180d66d6ae373eac1144cb
unknown <arpadneale@gmail.com>
2018-12-08 19:15:27 -0500

    added a db_test.py to interact with the database instance and test

---
+++ db_test.py
@@ -0,0 +1,17 @@
+from flask_server import db
+from flask_server.models import *
+import unittest
+
+'''
+PLEASE READ THIS BEFORE RUNNING!
+
+MAKE SURE THAT AN INSTANCE OF THE DATABASE HAS BEEN SET UP FIRST
+THIS CAN BE DONE FOR YOU BY RUNNING db_create.py first
+IT IS ALSO RECCOMNEDED BUT NOT NECCESSARY TO HAVE RUN db_clear.py TO CLEAR OUT
+THE DATABASE BEFORE RUNNING db_test.py
+'''
+
+if __name__ == '__main__':
+    unittest.main()
+
+#these are test methods to garuntee that the CRUD is working
