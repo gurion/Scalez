55629d4b6b91d6e80f5ef0c99160ee031d9e24de
Gurion Marks <gurion@jhu.edu>
2018-12-14 22:39:40 -0500

    remove merge marker

--- frontend/Scalez/Scalez/PendingAuditionsViewController.swift
+++ frontend/Scalez/Scalez/PendingAuditionsViewController.swift
@@ -12,24 +12,16 @@ import SwiftyJSON
 import Alamofire
 
 class PendingAuditionsViewController : UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
+
     @IBOutlet var pendingAuditions: UITableView!
     @IBOutlet var back: UIButton!
-    
-    @IBAction func reloadButton(_ sender: Any) {
-        self.getAuditions(completion: {
-            if self.auditionee.count + self.auditioner.count > 0 {
-                self.pendingAuditions.reloadData()
-            }
-        })
-    }
-    
+
     let sections = ["auditionee", "auditioner"]
     var auditionee = [[String : Any]]()
     var auditioner = [[String : Any]]()
     let cellReuseIdentifier = "Cell"
     var selectedIndex = 0
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.getAuditions(completion: {
@@ -40,15 +32,15 @@ class PendingAuditionsViewController : UIViewController, UITableViewDelegate, UI
         self.pendingAuditions.delegate   = self
         self.pendingAuditions.dataSource = self
     }
-    
+
     func numberOfSections(in tableView: UITableView) -> Int{
         return sections.count
     }
-    
+
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return sections[section] as? String
     }
-    
+
     @IBAction func backButton(_ sender: Any) {
              dismiss(animated: true, completion: nil)
     }
@@ -61,11 +53,11 @@ class PendingAuditionsViewController : UIViewController, UITableViewDelegate, UI
             return 0
         }
     }
-    
+
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            
+
         let cell:AuditionTableCell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath) as! AuditionTableCell
-        
+
         if indexPath.section == 0 {
             let audition = auditionee[indexPath.row]
             cell.usernameLabel.text = audition["auditioner"] as! String
@@ -83,10 +75,10 @@ class PendingAuditionsViewController : UIViewController, UITableViewDelegate, UI
             cell.isComplete = audition["isComplete"] as! Bool
             cell.audtionID = audition["id"] as! String
         }
-        
+
         return cell
     }
-    
+
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 
@@ -105,38 +97,35 @@ class PendingAuditionsViewController : UIViewController, UITableViewDelegate, UI
                 completeAuditionVC.auditionerUsernameLabel.text = "Auditioner: " + (auditionee[selectedIndex]["auditioner"] as! String)
                 completeAuditionVC.scaleLabel.text = "Scale: " + (auditionee[selectedIndex]["scale"] as! String)
                 completeAuditionVC.keyLabel.text = "Key: " + (auditionee[selectedIndex]["key"] as! String)
-<<<<<<< HEAD
-=======
         }
-        
+
         }
     }
-    
+
     @IBAction func reloadButton(_ sender: Any) {
         self.getAuditions(completion: {
             if self.auditionee.count + self.auditioner.count > 0 {
                 self.pendingAuditions.reloadData()
->>>>>>> 06bdf39... add functionality to push data over complete audition segue
             }
         }
     }
-    
+
     func okButtonAlert(title : String, message : String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
         self.present(alert, animated: true)
     }
-    
+
     func generalAlert() {
         self.okButtonAlert(title: "Something went wrong!", message: "Sorry! Please try again.")
     }
-    
+
     func getAuditions(completion : @escaping ()->()) {
         let url: String = UserDefaults.standard.string(forKey: "userUrl")!+"/audition"
         Alamofire.request(url).responseJSON { (responseData) -> Void in
             if((responseData.result.value) != nil) {
                 let jsonResponse = JSON(responseData.result.value!)
-                
+
                 if let array = jsonResponse["auditions"].arrayObject {
                     self.auditionee = array[0] as! [[String : Any]]
                     self.auditioner = array[1] as! [[String : Any]]
@@ -148,7 +137,7 @@ class PendingAuditionsViewController : UIViewController, UITableViewDelegate, UI
             }
             completion()
         }
-        
+
     }
-    
+
 }
